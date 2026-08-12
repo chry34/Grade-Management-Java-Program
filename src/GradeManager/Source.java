97f5a041587f509bbbac52065de6d2ede7bf3f76
chry34 <morenochristle4@gmail.com>
2026-08-12 20:18:16 +0800

    Add files via upload

--- src/GradeManager/Source.java
+++ src/GradeManager/Source.java
@@ -1,65 +1,70 @@
 package GradeManager;
 import java.util.Scanner;
-import java.util.Array;
-import java.util.Collections;
+//import java.util.Array;
+//import java.util.Collections;
 
 public class Source {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		System.out.println("===== Grade Management System =====");
 		
-		// Number of Students
+		// pangalan nila
 		System.out.print("Enter the Number of Students: ");
 		int studentAmount = input.nextInt();
 		input.nextLine();
 		System.out.print("Enter the Total Score of Exam: ");
-		int scoreAmount = input.nextInt();
+		double totalScore = input.nextInt();
 		input.nextLine();
 
-		// Array Storage
+		// arrays
         String[] names = new String[studentAmount];
         double[] score = new double[studentAmount];
-        		
-        int i = 0;
-        int x = 0;
+     
         
-        // Input Name and Grade
-        for (i = 0; i < studentAmount; i++) {
+        // name and grade whahaha
+        for (int i = 0; i < studentAmount; i++) {
             System.out.print("Enter name of student #" + (i + 1) + ": ");
             names[i] = input.nextLine();
             	
             System.out.print("Enter Score of Student #" + (i + 1) + ": ");
             score[i] = input.nextDouble();
             input.nextLine();
-            
-//    		double[i] scorePercentage = (score / scoreAmount) * 100;
             }
 
-        	// secret thing
+        	// secret thing wag mona pansinin
         	for (int z = 0; z < 129; z++) {
         	System.out.println(" ");
         	}
             
-            System.out.println("---- Student Information ----");
-            for (x = 0; x < studentAmount; x++) {
-
-        		
+        	// student info
+            System.out.println("====== Student Information =====");
+            for (int x = 0; x < studentAmount; x++) {
+            	double scorePercentage = (score[x]/totalScore)*100;
  				System.out.println("Name: " + names[x]);
- //				System.out.println("Score: " + scorePercentage);
- 				System.out.println("Grade: " + score[x]);
- 				System.out.println(" ");
+ 				System.out.println("Score: " + score[x]);
+ 				
+ 				if (scorePercentage >= 75) {
+ 					System.out.println("Status: PASSED");
+ 					System.out.println(" ");
+ 				} else {
+ 					System.out.println("Status: FAILED");
+ 					System.out.println(" ");
+ 				}
+ 				
  			}
             
+            System.out.println(" ");
+            
+            // sort (magulo to ah)
             for (int t = 0; t < studentAmount - 1; t++) {
                 for (int j = t + 1; j < studentAmount; j++) {
 
                     if (score[t] < score[j]) {
 
-                        // Swap score
                         double tempGrade = score[t];
                         score[t] = score[j];
                         score[j] = tempGrade;
 
-                        // Swap names too
                         String tempName = names[t];
                         names[t] = names[j];
                         names[j] = tempName;
@@ -67,19 +72,18 @@ public class Source {
                 }
             }
             
+            // rank na at total average
             System.out.println("===== Ranking =====");
             
             for (int r = 0; r < studentAmount; r++) {
-                System.out.println("Rank " + (r + 1) + ": " + names[r] + " - " + score[r]);
-                
-                if (score[r] >= 75) {
-                    System.out.println("PASSED");
-                    System.out.println("");
-                } else {
-                    System.out.println("FAILED");
-                    System.out.println("");
-                }
+            	double scoreConvert = (score[r]/totalScore)*100;
+            	if (scoreConvert >= 75  ) {
+                System.out.println("Rank " + (r + 1) + ": " + names[r] + " - " + score[r] + "/" + totalScore);
+                System.out.println("Average: " + scoreConvert + "%");
+                System.out.println(" ");
+            	} else {
+                    System.out.print("");
+            	}
             }
 	}
-
 }
