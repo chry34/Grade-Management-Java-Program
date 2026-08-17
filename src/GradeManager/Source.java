7f243362820fa024d95d1b90f45d2e6935a3cd8c
chry34 <morenochristle4@gmail.com>
2026-08-17 09:13:40 +0800

    Update Source.java
    added ordinal
    fixed rank list
    deleted useless lines
    removed disregarded comments
    fixed aral program list

--- src/GradeManager/Source.java
+++ src/GradeManager/Source.java
@@ -1,111 +1,117 @@
 package GradeManager;
 import java.util.Scanner;
-//import java.util.Array;
-//import java.util.Collections;
 
 public class Source {
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("===== Grade Management System =====");
-		
-		// pangalan nila and total score
-		System.out.print("Enter the Number of Students: ");
-		int studentAmount = input.nextInt();
-		input.nextLine();
-		System.out.print("Enter the Total Score of Exam: ");
-		double totalScore = input.nextInt();
-		input.nextLine();
+	 public static String getOrdinal(int num) {
+		 
+		 // declare ordinal para sa rank
+	        if (num >= 11 && num <= 13) return num + "th";
+	        switch (num % 10) {
+	            case 1: return num + "st";
+	            case 2: return num + "nd";
+	            case 3: return num + "rd";
+	            default: return num + "th";
+	        }
+	    }
+	 	
+	 	// store num of students and declare score of exam
+	    public static void main(String[] args) {
+	        Scanner input = new Scanner(System.in);
+	        System.out.println("===== Grade Management System =====");
+	        
+	        System.out.print("Enter the Number of Students: ");
+	        int studentAmount = input.nextInt();
+	        input.nextLine();
+	        System.out.print("Enter the Total Score of Exam: ");
+	        double totalScore = input.nextDouble();
+	        input.nextLine();
 
-		// arrays o set of data
-        String[] names = new String[studentAmount];
-        double[] score = new double[studentAmount];
-     
-        
-        // name and grade
-        for (int i = 0; i < studentAmount; i++) {
-            System.out.print("Enter name of student #" + (i + 1) + ": ");
-            names[i] = input.nextLine();
-            	
-            System.out.print("Enter Score of Student #" + (i + 1) + ": ");
-            score[i] = input.nextDouble();
-            input.nextLine();
-            }
-
-        	//legends lang nakakaalam
-        	for (int z = 0; z < 129; z++) {
-        	System.out.println(" ");
-        	}
-            
-        	// student info
-            System.out.println("====== Student Information =====");
-            for (int x = 0; x < studentAmount; x++) {
+	        String[] names = new String[studentAmount];
+	        double[] score = new double[studentAmount];
+	        
+	        for (int i = 0; i < studentAmount; i++) {
+	            System.out.print("Enter name of student #" + (i + 1) + ": ");
+	            names[i] = input.nextLine();
+	            System.out.print("Enter Score of Student #" + (i + 1) + ": ");
+	            score[i] = input.nextDouble();
+	            input.nextLine();
+	        }
+	        
+	        // legends lang nakakaalam
+	        for (int z = 0; z < 129; z++) System.out.println();
+	        
+	        // info and status of students
+	        System.out.println("====== Student Information =====");
+	        for (int x = 0; x < studentAmount; x++) {
             	double scorePercentage = (score[x]/totalScore)*100;
  				System.out.println("Name: " + names[x]);
- 				System.out.println("Score: " + score[x]);
+ 				System.out.println("Score: " + score[x] + "/" + totalScore);
  				
  				if (scorePercentage >= 99) {
  					System.out.println("Status: PASSED with Highest Honor");
- 					System.out.println(" ");
  				} else if (scorePercentage >= 95) {
  					System.out.println("Status: PASSED with High Honor");
- 					System.out.println(" ");
  				} else if (scorePercentage >= 90) {
  					System.out.println("Status: PASSED with Honor");
- 					System.out.println(" ");
  				} else if (scorePercentage >= 75) {
  					System.out.println("Status: PASSED");
- 					System.out.println(" ");
  				} else {
  	 				System.out.println("Status: FAILED");
- 	 				System.out.println(" ");
  				}
- 				
- 			}
-            
-            System.out.println(" ");
-            
-            // sort (magulo to ah)
-            for (int t = 0; t < studentAmount - 1; t++) {
-                for (int j = t + 1; j < studentAmount; j++) {
+	            System.out.println(" ");
+	        }
+	        
+	        // magulo to
+	        for (int t = 0; t < studentAmount - 1; t++) {
+	            for (int j = t + 1; j < studentAmount; j++) {
+	                if (score[t] < score[j]) {
+	                    double tempGrade = score[t];
+	                    score[t] = score[j];
+	                    score[j] = tempGrade;
+	                    
+	                    String tempName = names[t];
+	                    names[t] = names[j];
+	                    names[j] = tempName;
+	                }
+	            }
+	        }
+	        
+	        // rank na
+	        System.out.println("===== RANK =====\n");
+	        int rank = 1;
+	        int i = 0;
+	        while (i < studentAmount) {
+	            double percent = (score[i] / totalScore) * 100;
+	            
+	            if (percent >= 75) { 	
+	                System.out.println(getOrdinal(rank) + " Place");
 
-                    if (score[t] < score[j]) {
+	                int j = i;
+	                while (j < studentAmount && score[j] == score[i]) {
+	                    System.out.println(names[j] + " - " + percent + "%");
+	                    j++;
+	                }
+	                
+	                System.out.println();
+	                rank = rank + 1; 
+	                i = j; 
+	            } else {
+	                i++;
+	            }
+	        }
+	        
+	        // mga kawawa
+	        System.out.println("===== Aral Program List =====");
+	        System.out.println(" ");
+	        for (int r = 0; r < studentAmount; r++) {
+	           	double scoreConvert = (score[r]/totalScore)*100;
 
-                        double tempGrade = score[t];
-                        score[t] = score[j];
-                        score[j] = tempGrade;
-
-                        String tempName = names[t];
-                        names[t] = names[j];
-                        names[j] = tempName;
-                    }
-                }
-            }
-            
-            // rank at total average of students
-            System.out.println("===== Ranking =====");
-            for (int r = 0; r < studentAmount; r++) {
-            	double scoreConvert = (score[r]/totalScore)*100;
-            	
-      
-            	if (scoreConvert >= 75  ) {
-                System.out.println("Rank " + (r + 1) + ": " + names[r] + " - " + score[r] + "/" + totalScore);
+            	if (scoreConvert <= 75) {
+                System.out.println("Name: " + names[r]);
+                System.out.println("Score Achieved: " + score[r] + "/" + totalScore);
                 System.out.println("Average Achieved: " + scoreConvert + "%");
                 System.out.println(" ");
-            	} else {
-                    System.out.print(""); 
-            	}
-            }
-            
-         // mga kawawa
-            System.out.println("===== Aral Program List =====");
-            for (int r = 0; r < studentAmount; r++) {
-            	double scoreConvert = (score[r]/totalScore)*100;
-
-            	if (scoreConvert <= 75  ) {
-                System.out.println("Name: " + names[r] + " - " + score[r] + "/" + totalScore);
-                System.out.println("Average Achieved: " + scoreConvert + "%");
-                System.out.println(" ");
-            	}
-            }
-	}
+	            }
+	        }
+		}
 }
